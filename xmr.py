f14eace4a951b020e397652addd08f56c19ea921
Jack Murray <jack@tradeapi.net>
2022-12-26 20:50:56 +0000

    make xmr rpc calls more resilient

--- xmr.py
+++ xmr.py
@@ -1,6 +1,7 @@
 from monero.backends.jsonrpc import JSONRPCWallet
 from monero.wallet import Wallet
 from env import MONERO_RPC_PASSWORD
+from time import sleep
 
 MINCONF = 15
 
@@ -9,21 +10,42 @@ def connect():
 
 class XMR:
   def height(self):
-    wallet = connect()
-    return wallet.height() - MINCONF
+    while True:
+      try:
+        wallet = connect()
+        return wallet.height() - MINCONF
+      except:
+        sleep(1)
 
   def get_incoming_txs(self, height):
-    wallet = connect()
-    txs = wallet.incoming(min_height=height, max_height=height)
-    return [(tx.local_address, tx.amount) for tx in txs]
+    while True:
+      try:
+        wallet = connect()
+        txs = wallet.incoming(min_height=height, max_height=height)
+        return [(tx.local_address, tx.amount) for tx in txs]
+      except:
+        sleep(1)
 
   def withdraw(self, address, amount):
-    wallet = connect()
-    wallet.transfer(address, amount)
+    while True:
+      try:
+        wallet = connect()
+        wallet.transfer(address, amount)
+      except ValueError:
+        raise ValueError
+      except:
+        sleep(1)
 
   def get_new_deposit_address(self):
-    wallet = connect()
-    return wallet.new_address()[0]
+    while True:
+      try:
+        wallet = connect()
+        return wallet.new_address()[0]
+      except:
+        sleep(1)
 
   def minimum_withdrawal(self):
     return Decimal('0.0001')
+
+  def round_down(self, amount):
+    return floor(amount * 10**12) / Decimal(10**12)
