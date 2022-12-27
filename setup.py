a0eaa1f489f32d69dab52399f4353d455dd7e3b2
Jack Murray <jack@tradeapi.net>
2022-12-27 22:12:42 +0000

    fix typo bug

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
   engine = create_engine(DB)
   Base.metadata.create_all(engine)
   session = Session(engine)
-  session.add(Asset(name='BTC', height=768763)))
+  session.add(Asset(name='BTC', height=768763))
   session.add(Asset(name='XMR', height=2784543))
   session.commit()
