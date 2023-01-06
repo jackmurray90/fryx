73a8c5d5e7a925ff02e590387d869326e0c3b6dd
Jack Murray <jack@tradeapi.net>
2023-01-06 18:01:05 +0000

    remove recaptcha secret from env.example.py

--- env.example.py
+++ env.example.py
@@ -1,4 +1,3 @@
-RECAPTCHA_SECRET = '... reCAPTCHA secret here...'
 DB = 'postgresql://username:password@localhost/tradeapi'
 TEST_DB = 'postgresql://username:password@localhost/tradeapi_test'
 BITCOIN = 'http://user:pass@127.0.0.1:8332'
