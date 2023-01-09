f06ad563322da4ca15fe995dce6d6fa6ce7d427f
Jack Murray <jack@fryx.finance>
2023-01-09 10:18:56 +0000

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from db import Base, Asset, Market, User
+from assets import assets
 from env import DB
 
 if __name__ == '__main__':
   engine = create_engine(DB)
   Base.metadata.create_all(engine)
   session = Session(engine)
-  session.add(Market(name='BTCXMR', asset=Asset(name='XMR', height=2784543), currency=Asset(name='BTC', height=768763)))
+  session.add(Market(name='BTCXMR',
+    asset=Asset(name='XMR', height=asset['XMR'].height()),
+    currency=Asset(name='BTC', height=asset['BTC'].height())))
   session.add(User(api_key='auto'))
   session.commit()
