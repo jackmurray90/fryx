741d2a6069fc32897657c47dc9466dfe8507be05
Jack Murray <jack@tradeapi.net>
2023-01-10 14:27:11 +1100

    rename tradeapi to fryx in env.example.py

--- env.example.py
+++ env.example.py
@@ -1,4 +1,4 @@
-DB = 'postgresql://username:password@localhost/tradeapi'
-TEST_DB = 'postgresql://username:password@localhost/tradeapi_test'
+DB = 'postgresql://username:password@localhost/fryx'
+TEST_DB = 'postgresql://username:password@localhost/fryx_test'
 BITCOIN = 'http://user:pass@127.0.0.1:8332'
 MONERO = 'http://user:pass@127.0.0.1:18081'
